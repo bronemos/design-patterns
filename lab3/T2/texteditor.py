6860ac2099281b37bfb1927249be0e3bf25e5782
bronemos <spieglb@gmail.com>
2021-05-13 20:31:37 +0200

    update t2

--- lab3/T2/texteditor.py
+++ lab3/T2/texteditor.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 
 
+class Location:
+    def __init__(self, row: int, column: int):
+        self.row = row
+        self.column = column
+
+
+class LocationRange:
+    def __init__(self, start: Location, end: Location):
+        self.start = start
+        self.end = end
+
+
 class TextEditorModel:
     def __init__(self, lines_string: str):
         self.lines = lines_string.split("\n")
-        self.selection_range = list()
-        self.cursor_location = list()
+        self.selection_range = Location(0, 0)
+        self.cursor_location = Location(0, 0)
 
 
-class TextEditor(tk.Frame):
+class TextEditor(tk.Tk):
     def __init__(self, text_editor_model: TextEditorModel):
+        super().__init__()
+        self.canvas = tk.Canvas()
         self.text_editor_model = text_editor_model
+        self.canvas.create_text(text="Test")
 
 
 def main():
-    pass
+    model = TextEditorModel("test\ntest\ntest")
+    editor = TextEditor(model)
+    editor.mainloop()
 
 
 if __name__ == "__main__":
